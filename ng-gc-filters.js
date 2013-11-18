9d3863a556d22f4e20eabc4eb501de01f128a2d6
Andrew Appleton <andrew@floatleft.com>
2013-11-18 16:22:03 +0000

    Include new filter in the module

--- ng-gc-filters.js
+++ ng-gc-filters.js
@@ -9,5 +9,6 @@ angular.module('ngGcFilters', [
   'ngGcParseFloat',
   'ngGcSafeString',
   'ngGcToDateTime',
-  'ngGcUnderscoreToSpace'
+  'ngGcUnderscoreToSpace',
+  'ngGcCurrencySymbolFilter'
 ]);
